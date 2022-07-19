48e33469ad347cc5624a44ccda5de4e8f9de1d70
HUYNH KIM HIEU <89566149+goldenace21@users.noreply.github.com>
2022-07-19 09:47:45 +0700

    up

--- _04_angular_component_template/practice/angular-hackernews-app/src/app/app.module.ts
+++ _04_angular_component_template/practice/angular-hackernews-app/src/app/app.module.ts
@@ -3,26 +3,26 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ArticleComponent } from './article/article.component';
+import {FormsModule} from "@angular/forms";
+import { LikeComponent } from './like/like.component';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
-import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent,
-    FooterComponent,
-    HomeComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  exports: [
+    ArticleComponent,
+    LikeComponent,
     HeaderComponent,
     FooterComponent
   ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule
+  ],
+  providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
