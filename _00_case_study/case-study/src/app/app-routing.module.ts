32910317def831863ddbcd1695dc971ac01776a0
HUYNH KIM HIEU <89566149+goldenace21@users.noreply.github.com>
2022-07-20 13:34:49 +0700

    up

--- _00_case_study/case-study/src/app/app-routing.module.ts
+++ _00_case_study/case-study/src/app/app-routing.module.ts
@@ -6,6 +6,7 @@ import {FacilityComponent} from "./facility/facility.component";
 import {ContractComponent} from "./contract/contract.component";
 
 const routes: Routes = [
+  { path: "", pathMatch: "full", redirectTo: "home" },
   { path: "home", component: HomeComponent },
   { path: "customer", component: CustomerComponent },
   { path: "facility", component: FacilityComponent },
---
+++ _00_case_study/case-study/src/app/facility/facility.ts
@@ -0,0 +1,22 @@
+export class Facility {
+  id: number;
+  name: string;
+  area: string;
+  cost: string;
+  maxPeople: string;
+  rentType: number;
+
+}
+// private Integer id;
+// private String name;
+// private String area;
+// private String cost;
+// private String maxPeople;
+// private RentType rentType;
+// private String standardRoom;
+// private String description;
+// private String poolArea;
+// private String floorsNumber;
+// private String facilityFree;
+// private Integer deleteStatus;
+// private FacilityType facilityType;
